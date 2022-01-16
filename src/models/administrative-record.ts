9832c42aed340780acd112edb7ac961803040b43
Luis Sandoval <alfsand97@gmail.com>
2022-01-16 14:01:03 -0700

    first commit

---
+++ src/models/administrative-record.ts
@@ -0,0 +1,7 @@
+interface AdministrativeRecord {
+  start_year: string,
+  end_year: string,
+  description: string
+}
+
+export default AdministrativeRecord;
---
+++ src/models/commitee-record.ts
@@ -0,0 +1,9 @@
+interface CommiteeRecord {
+  name: string,
+  role: string,
+  start_date: Date,
+  end_date: Date,
+  active: boolean
+}
+
+export default CommiteeRecord;
