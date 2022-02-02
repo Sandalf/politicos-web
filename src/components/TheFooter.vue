5a51063b4f63121cd9ae825c0eccebd75a580791
Luis Sandoval <alfsand97@gmail.com>
2022-02-02 10:17:41 -0700

    chore: update repo link

--- src/components/TheFooter.vue
+++ src/components/TheFooter.vue
@@ -1,20 +1,25 @@
 <script lang="ts" setup>
-import { BrandGitlabIcon } from 'vue-tabler-icons'
+import { BrandGithubIcon } from 'vue-tabler-icons'
 </script>
 
 <template>
     <footer class="pb-20 relative block bg-gray-900">
         <div class="container mx-auto px-4 lg:pt-12 lg:pb-10">
-            <div class="flex flex-wrap mt-12 justify-center">
+            <div class="flex flex-wrap justify-center">
                 <div class="w-full lg:w-3/12 px-4 text-center">
                     <div
                         class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center mt-6"
                     >
-                        <BrandGitlabIcon />
+                        <BrandGithubIcon />
                     </div>
-                    <h6 class="text-xl mt-5 font-semibold text-white">
-                        GitLab
-                    </h6>
+                    <a
+                        href="https://github.com/Sandalf/politicos-web"
+                        target="_blank"
+                    >
+                        <h6 class="text-xl mt-5 font-semibold text-white">
+                            Github
+                        </h6>
+                    </a>
                     <p class="mt-2 mb-4 text-gray-500">
                         Contribuye haciendo un merge request
                     </p>
